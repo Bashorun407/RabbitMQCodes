20914be3886cf949ffa5c6c368678192b0041619
Akinbobola Oluwaseyi <boboladvat@gmail.com>
2022-01-24 20:45:01 +0100

    updated message with Lombok annotation after lombok extension was installed

--- src/main/java/com/akinovapp/directrabbitmq/publish/Message.java
+++ src/main/java/com/akinovapp/directrabbitmq/publish/Message.java
@@ -20,100 +20,101 @@ import lombok.ToString;
 public class Message {
 
     @Id
-    @GeneratedValue(strategy =GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int messageID;
     private String sender;
     private String messageBody;
     private Date messageDate;
 
-
-    public Message() {
-    }
-
-    public Message(int messageID, String sender, String messageBody, Date messageDate) {
-        this.messageID = messageID;
-        this.sender = sender;
-        this.messageBody = messageBody;
-        this.messageDate = messageDate;
-    }
-
-    public int getMessageID() {
-        return this.messageID;
-    }
-
-    public void setMessageID(int messageID) {
-        this.messageID = messageID;
-    }
-
-    public String getSender() {
-        return this.sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
-
-    public String getMessageBody() {
-        return this.messageBody;
-    }
-
-    public void setMessageBody(String messageBody) {
-        this.messageBody = messageBody;
-    }
-
-    public Date getMessageDate() {
-        return this.messageDate;
-    }
-
-    public void setMessageDate(Date messageDate) {
-        this.messageDate = messageDate;
-    }
-
-    public Message messageID(int messageID) {
-        setMessageID(messageID);
-        return this;
-    }
-
-    public Message sender(String sender) {
-        setSender(sender);
-        return this;
-    }
-
-    public Message messageBody(String messageBody) {
-        setMessageBody(messageBody);
-        return this;
-    }
-
-    public Message messageDate(Date messageDate) {
-        setMessageDate(messageDate);
-        return this;
-    }
-
-    // @Override
-    // public boolean equals(Object o) {
-    //     if (o == this)
-    //         return true;
-    //     if (!(o instanceof Message)) {
-    //         return false;
-    //     }
-    //     Message message = (Message) o;
-    //     return messageID == message.messageID && Objects.equals(sender, message.sender) && Objects.equals(messageBody, message.messageBody) && Objects.equals(messageDate, message.messageDate);
+    // public Message() {
     // }
 
-    // @Override
-    // public int hashCode() {
-    //     return Objects.hash(messageID, sender, messageBody, messageDate);
+    // public Message(int messageID, String sender, String messageBody, Date
+    // messageDate) {
+    // this.messageID = messageID;
+    // this.sender = sender;
+    // this.messageBody = messageBody;
+    // this.messageDate = messageDate;
     // }
 
-    @Override
-    public String toString() {
-        return "{" +
-            " messageID='" + getMessageID() + "'" +
-            ", sender='" + getSender() + "'" +
-            ", messageBody='" + getMessageBody() + "'" +
-            ", messageDate='" + getMessageDate() + "'" +
-            "}";
-    }
- 
+    // public int getMessageID() {
+    // return this.messageID;
+    // }
+
+    // public void setMessageID(int messageID) {
+    // this.messageID = messageID;
+    // }
+
+    // public String getSender() {
+    // return this.sender;
+    // }
+
+    // public void setSender(String sender) {
+    // this.sender = sender;
+    // }
+
+    // public String getMessageBody() {
+    // return this.messageBody;
+    // }
+
+    // public void setMessageBody(String messageBody) {
+    // this.messageBody = messageBody;
+    // }
+
+    // public Date getMessageDate() {
+    // return this.messageDate;
+    // }
+
+    // public void setMessageDate(Date messageDate) {
+    // this.messageDate = messageDate;
+    // }
+
+    // public Message messageID(int messageID) {
+    // setMessageID(messageID);
+    // return this;
+    // }
+
+    // public Message sender(String sender) {
+    // setSender(sender);
+    // return this;
+    // }
+
+    // public Message messageBody(String messageBody) {
+    // setMessageBody(messageBody);
+    // return this;
+    // }
+
+    // public Message messageDate(Date messageDate) {
+    // setMessageDate(messageDate);
+    // return this;
+    // }
+
+    // // @Override
+    // // public boolean equals(Object o) {
+    // // if (o == this)
+    // // return true;
+    // // if (!(o instanceof Message)) {
+    // // return false;
+    // // }
+    // // Message message = (Message) o;
+    // // return messageID == message.messageID && Objects.equals(sender,
+    // message.sender) && Objects.equals(messageBody, message.messageBody) &&
+    // Objects.equals(messageDate, message.messageDate);
+    // // }
+
+    // // @Override
+    // // public int hashCode() {
+    // // return Objects.hash(messageID, sender, messageBody, messageDate);
+    // // }
+
+    // @Override
+    // public String toString() {
+    // return "{" +
+    // " messageID='" + getMessageID() + "'" +
+    // ", sender='" + getSender() + "'" +
+    // ", messageBody='" + getMessageBody() + "'" +
+    // ", messageDate='" + getMessageDate() + "'" +
+    // "}";
+    // }
 
 }
